aa83f7649583d9a75fc36c0dead3d1c06c8eb07d
Moh Sopyan <mohsopyan29@gmail.com>
2026-02-04 19:42:05 +0900

    feat: add WITHDRAW action via policy configuration

--- policies/action-policies.js
+++ policies/action-policies.js
@@ -1,5 +1,5 @@
 module.exports = {
   REGISTER: ["activeRule", "ageRule"],
-  WITHDRAW: ["activeRule", "kycRule", "amlRule"],
+  WITHDRAW: ["activeRule", "kycRule"],
   TRANSFER: ["activeRule", "kycRule", "amlRule", "ageRule"],
 };
